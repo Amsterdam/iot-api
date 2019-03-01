b4ccf58319d6d1d90ccf2610f4e2ee6d5bcc8cb9
David van Buiten <david.vanbuiten+github@gmail.com>
2019-03-01 09:38:41 +0100

    Made the email backend hardcoded for now

--- api/src/iot/settings/settings.py
+++ api/src/iot/settings/settings.py
@@ -341,7 +341,8 @@ REST_FRAMEWORK = dict(
 
 
 # Mail
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'djcelery_email.backends.CeleryEmailBackend')
+# EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'djcelery_email.backends.CeleryEmailBackend')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
