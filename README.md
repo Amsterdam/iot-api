290854a3d99675727956b33a937e3515f930eba9
Hielke Kramer <kramerh@gmail.com>
2019-07-23 15:32:06 +0200

    Small readme edit and a little refactoring (no functional changes).

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
-# iot #
+# iot
+This project serves the data for [slimmeapparaten.amsterdam.nl](https://slimmeapparaten.amsterdam.nl/) for which the 
+frontend can be found over at [github.com/Amsterdam/register-slimme-apparaten-frontend](https://github.com/Amsterdam/).
+
+### Note to reader
+There is am import command in `management/commands/import_csv.py`, but it is unclear where in Jenkins 
+(or anywhere else) this command is run and what should be the data source. Can the future reader who 
+is _"in the know"_ be so kind as to add that information to this readme?
 
 
-### Install procedure ###
-
-```
-git clone Full git address of the repository (eq. https://github.com/Amsterdam/example.git) iot
-cd iot
-```
+### How to run
 
 Start the docker containers manually
 ```
 docker-compose up -d
 ```
 
-#### Local development ####
+#### Local development
 
 To create the virtualenvironment (python3) and install requirements run:
 ```
--- api/src/iot/models.py
+++ api/src/iot/models.py
@@ -9,14 +9,8 @@ class Type(models.Model):
     """
     The type
     """
-    name = models.CharField(
-        max_length=64,
-    )
-
-    application = models.CharField(
-        max_length=64,
-    )
-
+    name = models.CharField(max_length=64)
+    application = models.CharField(max_length=64)
     description = models.TextField()
 
 
@@ -24,83 +18,29 @@ class Person(models.Model):
     """
     The owner/contact person
     """
-    name = models.CharField(
-        max_length=255,
-    )
-
+    name = models.CharField(max_length=255)
     email = models.EmailField()
-
-    organisation = models.CharField(
-        max_length=250,
-        null=True,
-        blank=True,
-    )
+    organisation = models.CharField(max_length=250, null=True, blank=True)
 
 
 class Device(models.Model):
     """
     The iot device "thing"
     """
-    reference = models.CharField(
-        max_length=64,
-    )
-    application = models.CharField(
-        max_length=64,
-    )
-    types = models.ManyToManyField(
-        to='Type',
-    )
-    categories = MultiSelectField(
-        choices=CATEGORY_CHOICES,
-        max_length=64,
-        max_choices=6,
-        null=True,
-        blank=True,
-    )
-    installation_point = models.CharField(
-        max_length=64,
-        null=True,
-    )
-    frequency = models.CharField(
-        max_length=16,
-        choices=FREQUENCY_CHOICES,
-        null=True,
-    )
-    permit = models.BooleanField(
-        default=False,
-        null=True,
-        blank=True,
-    )
-    in_use_since = models.DateField(
-        null=True,
-        blank=True,
-    )
+    reference = models.CharField(max_length=64)
+    application = models.CharField(max_length=64)
+    types = models.ManyToManyField(to='Type')
+    categories = MultiSelectField(choices=CATEGORY_CHOICES, max_length=64, max_choices=6, null=True, blank=True)
+    installation_point = models.CharField(max_length=64, null=True)
+    frequency = models.CharField(max_length=16, choices=FREQUENCY_CHOICES, null=True)
+    permit = models.BooleanField(default=False, null=True, blank=True)
+    in_use_since = models.DateField(null=True, blank=True)
 
     # Postal code, housenumber and location
-    postal_code = models.CharField(
-        max_length=6,
-        null=True,
-        blank=True,
-    )
-    house_number = models.CharField(
-        max_length=8,
-        null=True,
-        blank=True,
-    )
+    postal_code = models.CharField(max_length=6, null=True, blank=True)
+    house_number = models.CharField(max_length=8, null=True, blank=True)
     geometrie = gis_models.PointField(name='geometrie', null=True, blank=True)
 
     # Owner and contact
-    owner = models.ForeignKey(
-        to='Person',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='owner',
-    )
-    contact = models.ForeignKey(
-        to='Person',
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='contact',
-    )
+    owner = models.ForeignKey(to='Person', on_delete=models.SET_NULL, null=True, blank=True, related_name='owner')
+    contact = models.ForeignKey(to='Person', on_delete=models.SET_NULL, null=True, blank=True, related_name='contact')
