45d44d2f61050b8004a1697355748f75fa6e412c
Daniel Bradburn <moagstar@gmail.com>
2021-11-25 14:47:00 +0100

    Fix problem with use of staff_member_required decorator

--- api/src/iot/admin.py
+++ api/src/iot/admin.py
@@ -25,6 +25,31 @@ class FileForm(forms.Form):
     selecteer_bestand = forms.FileField()
 
 
+@staff_member_required
+def import_xlsx_view(request, message_user):
+    if request.method == "POST":
+        try:
+            file = request.FILES["selecteer_bestand"]
+            workbook = load_workbook(file)
+            errors, created, updated = import_xlsx(workbook)
+            if errors:
+                for e in errors:
+                    message_user(request, mark_safe(str(e)), level=messages.ERROR)
+            else:
+                # TODO: information about what was imported
+                for sensor in created:
+                    message_user(request, f"Sensor met referentie"
+                                          f" {sensor.reference} aangemaakt")
+                for sensor in updated:
+                    message_user(request, f"Sensor met referentie"
+                                          f" {sensor.reference} bijgewerkt")
+        except Exception as e:
+            message_user(request, mark_safe(str(e)), level=messages.ERROR)
+        return redirect("..")
+    else:
+        return render(request, "import_xlsx.html", {"form": FileForm()})
+
+
 @admin.register(models.Device2)
 class DeviceAdmin(LeafletGeoAdmin):
 
@@ -35,32 +60,12 @@ class DeviceAdmin(LeafletGeoAdmin):
     search_fields = 'reference', 'owner__organisation', 'owner__email', 'owner__name'
 
     def get_urls(self):
-        import_xlsx_path = path('import_xlsx/', self.import_xlsx_view)
+        import_xlsx_path = path(
+            'import_xlsx/',
+            import_xlsx_view,
+            dict(message_user=self.message_user),
+        )
         return [import_xlsx_path] + super().get_urls()
-
-    @staff_member_required
-    def import_xlsx_view(self, request):
-        if request.method == "POST":
-            try:
-                file = request.FILES["selecteer_bestand"]
-                workbook = load_workbook(file)
-                errors, created, updated = import_xlsx(workbook)
-                if errors:
-                    for e in errors:
-                        self.message_user(request, mark_safe(str(e)), level=messages.ERROR)
-                else:
-                    # TODO: information about what was imported
-                    for sensor in created:
-                        self.message_user(request, f"Sensor met referentie"
-                                                   f" {sensor.reference} aangemaakt")
-                    for sensor in updated:
-                        self.message_user(request, f"Sensor met referentie"
-                                                   f" {sensor.reference} bijgewerkt")
-            except Exception as e:
-                self.message_user(request, mark_safe(str(e)), level=messages.ERROR)
-            return redirect("..")
-
-        return render(request, "import_xlsx.html", {"form": FileForm()})
 
 
 class DeviceInline(LeafletGeoAdminMixin, admin.StackedInline):
