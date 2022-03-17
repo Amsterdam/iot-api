9b85a7ece07defa04a05b62f283c01a245bba2ac
Omar Aljazairy <fedal@Omars-MacBook-Pro.local>
2022-03-17 11:42:57 +0100

    removed the mappers and urls and the try/exception block
    git status

--- api/src/iot/management/commands/import_api.py
+++ api/src/iot/management/commands/import_api.py
@@ -2,27 +2,23 @@ from django.core.management.base import BaseCommand
 
 from iot import import_utils_apis
 
-API = 'https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?'
-API_MAPPER = {
-    'wifi_sensor_crowd_management': f'{API}KAARTLAAG=CROWDSENSOREN&THEMA=cmsa',
-    'camera_brug_en_sluisbediening': f'{API}KAARTLAAG=PRIVACY_BRUGSLUIS&THEMA=privacy',
-    # 'cctv_camera_verkeersmanagement': f'{API}KAARTLAAG=VIS&THEMA=vis',
-    # 'kentekencamera_reistijd': f'{API}KAARTLAAG=VIS&THEMA=vis',
-    # 'kentekencamera_milieuzone': f'{API}KAARTLAAG=VIS&THEMA=vis',
-    # 'ais_masten': f'{API}KAARTLAAG=PRIVACY_AISMASTEN&THEMA=privacy',
-    # 'verkeersonderzoek_met_cameras': f'{API}KAARTLAAG=PRIVACY_OVERIG&THEMA=privacy',
-    # 'beweegbare_fysieke_afsluiting': f'{API}KAARTLAAG=VIS_BFA&THEMA=vis',
-}
-
 
 class Command(BaseCommand):
     """
     Used by the django manage app for executing a function using the command line.
-    an argument can be provided with the name of the api to make an import only
-    for that api. If no argument is given, all the apis in the api_mapper will
-    be executed.
+    an argument should be provided with the name of the api to make an import only
+    for that api. it can also take a list of apis and all will be execute. The current
+    available apis are:
+    wifi_sensor_crowd_management
+    camera_brug_en_sluisbediening
+    cctv_camera_verkeersmanagement
+    kentekencamera_reistijd
+    kentekencamera_milieuzone
+    ais_masten
+    verkeersonderzoek_met_cameras
+    beweegbare_fysieke_afsluiting
     """
-    help = 'Imports Sensors data from APIs'
+    help = 'Imports Sensors data from APIs.To exeucting it: python manage.py import_api api1 api2'
 
     def add_arguments(self, parser):
         parser.add_argument('api_name', nargs='+', type=str, help='provide the api_name')
@@ -33,14 +29,9 @@ class Command(BaseCommand):
         be executed.
         """
 
-        try:
-            result_list = []  # empty list to save the apis results
-            for api_name in options['api_name']:
-                result = import_utils_apis.import_api_data(
-                    api_name=api_name,
-                    api_url=API_MAPPER[api_name]
-                )
-                result_list.append(result)  # append the import response as a tuple
-            self.stdout.write(self.style.SUCCESS(f'API IMPORT RESULT: {result_list}'))
-        except Exception as e:
-            self.stdout.write(self.style.ERROR_OUTPUT(f'API {e} NOT FOUND ERROR'))
+        result_list = []  # empty list to save the apis results
+        for api_name in options['api_name']:
+            result = import_utils_apis.import_api_data(api_name=api_name)
+            result_list.append(result)  # append the import response as a tuple
+
+        self.stdout.write(self.style.SUCCESS(f'{result_list}'))
