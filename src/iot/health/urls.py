0937d1216fcf97340c266ba96375bace1fadb2a1
Niels Lensink <nielslensink@gmail.com>
2023-01-24 19:07:29 +0100

    Be a bit more flexible with the health checks

--- src/iot/health/urls.py
+++ src/iot/health/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    path(r'health/', views.health, name='health'),
+    re_path(r'health/?', views.health, name='health'),
 ]
